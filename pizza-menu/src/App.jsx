b34a27fe6d1281204afab306856f76cf5ce396b2
Alex <87208475+PuSh0chek@users.noreply.github.com>
2024-01-30 16:55:00 +0300

    Добавил header, main, footer и заполнил контентом

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -1,19 +1,61 @@
 import './App.css';
+import './data.js'
 
-function App() {
+function App({content}) {
   return (
     <div className="App">
-      <Pizza />
+      <Header />
+      <Menu content={content} />
+      <Footer />
     </div>
   );
 }
 
-const Pizza = () => {
+const Header = () => {
+
+  const style = {color:"red", fontSize:"48px", textTransform:"uppercase"}
+
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    <header>
+      <h1 style={style} className="header">Fast React Pizza Co.</h1>
+    </header>
+  )
+}
+
+const Menu = ({content}) => {
+  return (
+    <main className='menu'>
+      <h2>Our menu</h2>
+      <div className='pizzas'>
+      {
+        content.map((item) => {
+          <Pizza content={item}/>
+        })
+      }
+      </div>
+    </main>
+  )
+}
+
+const Footer = () => {
+
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour <= closeHour) alert("Were're currently open!")
+  // else alert("Sorry we're closed")
+
+  return <footer className='footer'>{new Date().toLocaleTimeString()}. We're currently open</footer>
+}
+
+const Pizza = ({name, ingredients, price, photoName, soldOut}) => {
+  return (
+    <div className='{ soldOut ? {pizza} : {pizza, pizza.sold-out} }'>
+      <img src={photoName} alt={name} />
+      <h3>{name}</h3>
+      <p>{ingredients}</p><p>{price}$</p>
     </div>
   )
 }
--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -49,8 +49,4 @@ const pizzaData = [
 ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App content={pizzaData} />);
