29e42121f4ffd9df86ae843d08172ce56ded7000
Alex <87208475+PuSh0chek@users.noreply.github.com>
2024-02-02 15:32:24 +0300

    Проект Steps DONE.

--- steps/src/App.jsx
+++ steps/src/App.jsx
@@ -8,26 +8,45 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  )
+}
 
-  const [step, setStep] = useState(0);
+const Steps = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
-  const count = 1;
+  const handlePrevious = () => {
+    if(step > 1) setStep((s) => s - 1)
+  }
+
+  const handleNext = () => {
+    if(step < 3) setStep((s) => s + 1)
+  }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 1 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 1 ? 'active' : ''}`}>3</div>
-      </div>
+    <div>
+      <button type="button" className='close' onClick={() => setIsOpen((is) =>!is)}>&times;</button>
+      { isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
 
-      <p className="message">Step {step + 1}: {messages[step]}</p>
+          <p className="message">Step {step}: {messages[step - 1]}</p>
 
-      <div className='buttons'>
-        <button  style={{backgroundColor:"#7950f2", color: "#fff"}}>Previous</button>
-        <button  style={{backgroundColor:"#7950f2", color: "#fff"}}>Next</button>
-      </div>
-    </div>
-  );
+          <div className='buttons'>
+            <button onClick={handlePrevious} style={{backgroundColor:"#7950f2", color: "#fff"}}>Previous</button>
+            <button onClick={handleNext} style={{backgroundColor:"#7950f2", color: "#fff"}}>Next</button>
+          </div>
+        </div>)
+      }
+    </div>);
 }
 
