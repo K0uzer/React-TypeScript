3cbc299c65109a3321de0e9862a9c152c438ce5f
Alex <87208475+PuSh0chek@users.noreply.github.com>
2024-02-05 16:51:36 +0300

    Добавит верстку для проекта travel-list

--- travel-list/src/App.tsx
+++ travel-list/src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
-function App() {
+interface Task {
+  readonly id: number,
+  task: string,
+  ready: boolean,
+}
+
+const tasks: Task[] = [
+  {
+    id: 1,
+    task: 'Купить воды',
+    ready: false,
+  },
+]
+
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <Logo />
+      <Form />
+      <ContentList />
+      <Statistics />
+    </section>
   );
 }
 
-export default App;
+const Logo = () => {
+  return (
+    <h1>💕Far Away🎶</h1>
+  )
+}
+
+const Form = () => {
+  return (
+    <section className='add-form'>
+      <h3>What do you need for your 🤩 trip?</h3>
+    </section>
+  )
+}
+
+const ContentList = () => {
+  return (
+    <section className='list'>
+      <ul>{tasks.map((task:any) => <Item />)}</ul>
+    </section>
+  )
+}
+
+const Item = ({task, ready}:{task:string, ready:boolean}) => {
+  return (
+    <li style={{display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <p>{task}</p><button style={{cursor:'pointer', background: 'none'}}>✔️</button>
+    </li>
+  )
+}
+
+const Statistics = () => {
+
+
+
+  return (
+    <footer className='footer'>
+      🧮 You have {content.length} items on your list, and you already packed {} ({}%)
+    </footer>
+  )
+}
