7d2aae800801560ec0069373fda286dd5f772772
Alex <87208475+PuSh0chek@users.noreply.github.com>
2024-04-30 00:26:15 +0300

    Create, worck project `classy-weather` and delete project current-position

---
+++ classy-weather/src/Counter.js
@@ -0,0 +1,41 @@
+// import './App.css'
+
+// function App() {
+//     return <div className="App"></div>
+// }
+
+// export default App
+
+import React from 'react'
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { count: 5 }
+        this.handleDecrement = this.handleDecrement.bind(this)
+    }
+
+    handleDecrementDecrease() {
+        this.setState((curState) => {
+            return { count: curState.count - 1 }
+        })
+    }
+
+    handleDecrementIncrease() {
+        this.setState((curState) => {
+            return { count: curState.count + 1 }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleDecrementDecrease}>-</button>
+                <span>{this.state.count}</span>
+                <button onClick={this.handleDecrementIncrease}>++</button>
+            </div>
+        )
+    }
+}
+
+export default Counter
