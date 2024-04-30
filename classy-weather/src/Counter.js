3c7fd3604aba3068a705f0b7eb4ee2463f2deccd
Alex <87208475+PuSh0chek@users.noreply.github.com>
2024-04-30 20:05:12 +0300

    Done `Classy-weather`

--- classy-weather/src/Counter.js
+++ classy-weather/src/Counter.js
@@ -1,11 +1,3 @@
-// import './App.css'
-
-// function App() {
-//     return <div className="App"></div>
-// }
-
-// export default App
-
 import React from 'react'
 
 class Counter extends React.Component {
